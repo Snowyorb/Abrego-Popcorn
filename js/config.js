438c9ce0be72016958d9dfa69d681d50e1615e57
Snowyorb <36518860+Snowyorb@users.noreply.github.com>
2020-08-05 22:30:55 -0600

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-2_aRcpJEIUd',
+        userPoolClientId: '127aet6koopk3mu71uvkp4ag1i',
+        region: 'us-east-2'
+    },
+    api: {
+        invokeUrl: ''
+    }
+};
